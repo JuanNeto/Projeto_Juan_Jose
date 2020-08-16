3c574458c33dad587937e787c745ee871957fdfc
JuanNeto <56265592+JuanNeto@users.noreply.github.com>
2020-08-16 14:13:17 -0300

    Commit Inicial
    Classes A e B de acordo com o item 2 do trabalho.

---
+++ A.java
@@ -0,0 +1,28 @@
+public class A {
+    private int A1;
+    private float A2;
+
+    public int getA1() {
+        return A1;
+    }
+
+    public float getA2() {
+        return A2;
+    }
+
+    public void setA1(int novoA1) {
+        A1 = novoA1;
+    }
+
+    public void setA2(float novoA2) {
+        A2 = novoA2;
+    }
+
+    public void MA1() {
+        System.out.println("MA1");
+    }
+
+    public void MA2() {
+        System.out.println("MA1");
+    }
+}
---
+++ B.java
@@ -0,0 +1,28 @@
+public class B {
+    private int B1;
+    private float B2;
+
+    public int getB1() {
+        return B1;
+    }
+
+    public float getB2() {
+        return B2;
+    }
+
+    public void setB1(int novoB1) {
+        B1 = novoB1;
+    }
+
+    public void setB2(float novoB2) {
+        B2 = novoB2;
+    }
+
+    public void MB1() {
+        System.out.println("MB1");
+    }
+
+    public void MB2() {
+        System.out.println("MB2");
+    }
+}
